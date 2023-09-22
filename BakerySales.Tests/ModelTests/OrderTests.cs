cbb568202d6f1be612baac48088350414e1f0666
brnydr <brianwilliamyoder@gmail.com>
2023-09-22 14:36:51 -0700

    add Price property, 8th test pass

--- BakerySales.Tests/ModelTests/OrderTests.cs
+++ BakerySales.Tests/ModelTests/OrderTests.cs
@@ -11,7 +11,7 @@ namespace BakerySales.Tests
     [TestMethod]
     public void OrderConstructor_CreatesInstanceOfOrder_Order()
     {
-      Order newOrder = new Order("title", "description");
+      Order newOrder = new Order("title", "description", 9);
       Assert.AreEqual(typeof(Order), newOrder.GetType());
     }
 
@@ -19,7 +19,7 @@ namespace BakerySales.Tests
     public void GetDescription_ReturnsDescription_String()
     {
       string description = "new description";
-      Order newOrder = new Order("title", description);
+      Order newOrder = new Order("title", description, 9);
       Assert.AreEqual(description, newOrder.Description);
     }
 
@@ -27,8 +27,16 @@ namespace BakerySales.Tests
     public void GetTitle_ReturnsTitle_String()
     {
       string title = "new title";
-      Order newOrder = new Order(title, "description");
+      Order newOrder = new Order(title, "description", 9);
       Assert.AreEqual(title, newOrder.Title);
+    }
+
+    [TestMethod]
+    public void GetPrice_ReturnsPrice_Int()
+    {
+      int price = 9;
+      Order newOrder = new Order("title", "description", price);
+      Assert.AreEqual(price, newOrder.Price);
     }
 
   }
